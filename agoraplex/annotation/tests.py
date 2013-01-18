ba2786eddb6fe1c56422aabd639dadbb4c390133
Tripp Lilley <tripplilley@agoraplex.net>
2013-01-18 07:48:00 -0500

    rename annotation tests for consistency

--- agoraplex/annotation/tests.py
+++ agoraplex/annotation/tests.py
@@ -7,8 +7,8 @@ from agoraplex.annotation import annotated, returns, empty
 # ___TODO:___ check for non-tuples in default params on an @annotated
 # function
 
-class TestAnnotatedDecorater:
-    def test_no_params (self):
+class TestAnnotatedDecorater (object):
+    def test_without_params_without_annotations (self):
         @annotated
         def nop (): pass
 
@@ -18,7 +18,7 @@ class TestAnnotatedDecorater:
         # ``__annotations__`` should exist, but be empty
         assert len(nop.__annotations__) == 0
 
-    def test_params_without_annotations (self):
+    def test_with_params_without_annotations (self):
         @annotated
         def nop (a, b): pass
 
@@ -28,7 +28,7 @@ class TestAnnotatedDecorater:
         # ``__annotations__`` should exist, but be empty
         assert len(nop.__annotations__) == 0
 
-    def test_empty_annotations_without_defaults (self):
+    def test_with_params_without_defaults_empty_annotations (self):
         @annotated
         def nop (a=(empty,), b=(empty,)): pass
 
@@ -38,7 +38,7 @@ class TestAnnotatedDecorater:
         # ``__annotations__`` should exist, but be empty
         assert len(nop.__annotations__) == 0
 
-    def test_empty_annotations_with_defaults (self):
+    def test_with_params_with_defaults_empty_annotations (self):
         @annotated
         def nop (a=(empty,'alpha'), b=(empty,'beta')): pass
 
@@ -51,7 +51,7 @@ class TestAnnotatedDecorater:
         # ``__annotations__`` should exist, but be empty
         assert len(nop.__annotations__) == 0
 
-    def test_annotations_without_defaults (self):
+    def test_with_params_without_defaults_with_annotations (self):
         @annotated
         def nop (a=(int,), b=(unicode,)): pass
 
@@ -63,7 +63,7 @@ class TestAnnotatedDecorater:
         # ``__defaults__`` should be ``None``
         assert nop.__defaults__ is None
 
-    def test_annotations_with_defaults (self):
+    def test_with_params_with_defaults_with_annotations (self):
         @annotated
         def nop (a=(int, 'alpha'), b=(unicode, 'beta')): pass
 
@@ -77,7 +77,7 @@ class TestAnnotatedDecorater:
         assert nop.__defaults__[0] == 'alpha'
         assert nop.__defaults__[1] == 'beta'
 
-    def test_return_annotation_without_params (self):
+    def test_return_annotation_without_params_without_annotations (self):
         @annotated(returns=int)
         def nop (): pass
 
@@ -99,7 +99,7 @@ class TestAnnotatedDecorater:
         assert len(nop.__annotations__) == 1
         assert nop.__annotations__['return'] is int
 
-    def test_return_annotation_with_params_with_empty_annotations_without_defaults (self):
+    def test_return_annotation_with_params_without_defaults_with_empty_annotations (self):
         @annotated(returns=int)
         def nop (a=(empty,), b=(empty,)): pass
 
@@ -110,7 +110,7 @@ class TestAnnotatedDecorater:
         assert len(nop.__annotations__) == 1
         assert nop.__annotations__['return'] is int
 
-    def test_return_annotation_with_params_with_empty_annotations_with_defaults (self):
+    def test_return_annotation_with_params_with_defaults_with_empty_annotations (self):
         @annotated(returns=int)
         def nop (a=(empty,'alpha'), b=(empty,'beta')): pass
 
@@ -123,7 +123,7 @@ class TestAnnotatedDecorater:
         assert len(nop.__annotations__) == 1
         assert nop.__annotations__['return'] is int
 
-    def test_return_annotation_with_params_with_annotations_without_defaults (self):
+    def test_return_annotation_with_params_without_defaults_with_annotations (self):
         @annotated(returns=int)
         def nop (a=(int,), b=(unicode,)): pass
 
@@ -137,7 +137,7 @@ class TestAnnotatedDecorater:
         assert nop.__annotations__['b'] is unicode
 
 
-class TestReturnsDecorator:
+class TestReturnsDecorator (object):
     def test_return_annotation_without_params (self):
         @returns(int)
         def nop (): pass
@@ -170,7 +170,7 @@ class TestReturnsDecorator:
         assert len(nop.__annotations__) == 1
         assert nop.__annotations__['return'] is int
 
-    def test_return_empty_annotation (self):
+    def test_return_empty_annotation_without_params (self):
         @returns(empty)
         def nop (): pass
 
@@ -178,4 +178,14 @@ class TestReturnsDecorator:
         assert nop.__defaults__ is None
 
         # __annotations__ should exist, but be empty
+        assert len(nop.__annotations__) == 0
+
+    def test_return_empty_annotation_with_params (self):
+        @returns(empty)
+        def nop (a, b): pass
+
+        # ``__defaults__`` should be None
+        assert nop.__defaults__ is None
+
+        # ``__annotations__`` should exist, but be empty
         assert len(nop.__annotations__) == 0
