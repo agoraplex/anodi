ea91dc194b332705c97f27b9a548ab44ed836df8
Tripp Lilley <tripplilley@agoraplex.net>
2013-02-13 10:06:32 -0500

    consolidate requirements; generate pip files from that spec

--- setup.py
+++ setup.py
@@ -1,5 +1,30 @@
 from setuptools import setup
 
+requirements = {
+    'install': [
+        'distribute',
+        ],
+    'extras': {
+        'docs': [
+            'sphinx>=1.1',
+            'agoraplex.themes.sphinx>=0.1.3',
+            ],
+        'tests': [
+            'nose>=1.2.1',
+            'coverage>=3.6',
+            'pinocchio>=0.3.1',
+            'xtraceback>=0.3.3',
+            ],
+        },
+    }
+
+
+# write requirements for Travis and ReadTheDocs to use...
+with open("reqs/travis.txt", "w") as travis:
+    travis.write('\n'.join(requirements['extras']['tests']) + '\n')
+
+with open("reqs/rtfd.txt", "w") as rtfd:
+    rtfd.write('\n'.join(requirements['extras']['docs']) + '\n')
 
 setup(
     name='agoraplex.annotation',
@@ -12,5 +37,7 @@ setup(
     license='BSD',
     description='',
     long_description=open('README.rst').read(),
-    tests_require=['nose'],
+    install_requires=requirements.get('install', None),
+    tests_require=requirements.get('extras', {}).get('tests', None),
+    extras_require=requirements.get('extras', None),
 )
