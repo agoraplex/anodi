ae25d5e3a9786ff9eb3dceac0680f7ff67ba34c3
Tripp Lilley <tripplilley@gmail.com>
2013-02-13 14:04:41 -0500

    fix typo [ci skip]

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     namespace_packages=[],
     url='https://github.com/agoraplex/anodi',
     license='BSD',
-    description='A decorater-based backport of PEP-3107 function annotations to Python 2.7, and related tools',
+    description='A decorator-based backport of PEP-3107 function annotations to Python 2.7, and related tools.',
     long_description=open('README.rst').read(),
 
     install_requires=requirements.get('install', None),
